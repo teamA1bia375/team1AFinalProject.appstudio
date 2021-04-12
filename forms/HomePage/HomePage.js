0d53270be547a6bd212549f8bc6d0fb2cc16ceb9
Makala Smith <mms96354@creighton.edu>
2021-04-12 14:54:15 -0500

    done

--- forms/HomePage/HomePage.js
+++ forms/HomePage/HomePage.js
@@ -1,5 +1,5 @@
-// Global variables for database 
-let req = {} 
+// Global variables for database calls
+let req = {} // Indicates empty object
 let query = ""
 let results = []
 let pw = "ac@T!.........."  // put your database password here
@@ -8,7 +8,6 @@ let database = "375groupa1"
 let allUserData = []
 
 btnLogin.onclick=function(){
-//CHECKS IF USERNAME EXISTS
   let userName = ""
   userName = inptLogin.value
   query = "SELECT * FROM user WHERE userName = '" + userName + "'"
@@ -20,34 +19,14 @@ btnLogin.onclick=function(){
         if (results.length === 0)    
            lblMessage2.value = "USER NAME NOT FOUND"
         else {        
-      
-//CHECKS IF PASSWORD IS CORRECT 
-  let password = ""
-  userPassword = inptPassword.value
-  query = "SELECT * FROM user WHERE password = '" + userPassword + "'"
-  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + database + "&query=" + query)
-
-    if (req.status == 200) { //transit trip worked. 
-        results = JSON.parse(req.responseText)
-        console.log(`The results are /n ${results}`)
-        if (results.length === 0)    
-           lblMessage2.value = "INCORRECT PASSWORD"
-        else {        
       ChangeForm(UserPage)
-        } // end else
-
-    } else   // the transit didn't work - bad wifi? server turned off?
-        lblMessage.value = "Error code: " + req.status
-
-
-
         } // end else
 
     } else   // the transit didn't work - bad wifi? server turned off?
         lblMessage.value = "Error code: " + req.status
 }
 
-//GO TO SIGNIN PAGE
+
 btnUserInfo.onclick=function(){
   ChangeForm(UserInfo)
 }
