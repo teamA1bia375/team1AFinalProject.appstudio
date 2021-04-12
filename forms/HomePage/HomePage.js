ac882826752fd17b7c742506a98b172c554b2cc1
NoahSchalley <70349070+NoahSchalley@users.noreply.github.com>
2021-04-12 14:25:55 -0500

    Password

--- forms/HomePage/HomePage.js
+++ forms/HomePage/HomePage.js
@@ -19,7 +19,27 @@ btnLogin.onclick=function(){
         if (results.length === 0)    
            lblMessage2.value = "USER NAME NOT FOUND"
         else {        
+      
+
+  let password = ""
+  userPassword = inptPassword.value
+  query = "SELECT * FROM user WHERE password = '" + userPassword + "'"
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + database + "&query=" + query)
+
+    if (req.status == 200) { //transit trip worked. 
+        results = JSON.parse(req.responseText)
+        console.log(`The results are /n ${results}`)
+        if (results.length === 0)    
+           lblMessage2.value = "INCORRECT PASSWORD"
+        else {        
       ChangeForm(UserPage)
+        } // end else
+
+    } else   // the transit didn't work - bad wifi? server turned off?
+        lblMessage.value = "Error code: " + req.status
+
+
+
         } // end else
 
     } else   // the transit didn't work - bad wifi? server turned off?
